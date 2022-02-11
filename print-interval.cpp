bf7a860ae03922b947a16e568cd0f2c01135a6ae
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-11 01:12:28 +0000

    task 2 is started and in progress!

---
+++ print-interval.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//Write a program print-interval.cpp that asks the user to input 
+// two integers L and U (representing the lower and upper limits of the interval), 
+// and print out all integers in the range L ≤ i < U separated by spaces. 
+// Notice that we include the lower limit, but exclude the upper limit.
+int main()
+{
+  int L;
+  int U;
+  std::cout << "Input the value for L: ";
+  std::cin >> L;
+  std::cout << "Input the value for U: ";
+  std::cin >> U;
+
+  return 0;
+}
